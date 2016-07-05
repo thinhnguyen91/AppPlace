b470a80bc3051582e9937e46e8b40b6b23e79368
PhuongVNC <PhuongVNC@PhuongVNC.local>
2016-07-05 15:43:32 +0700

    Fix convention

--- Baitaplon/AppDelegate.swift
+++ Baitaplon/AppDelegate.swift
@@ -89,7 +89,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.rootViewController = navigation
     }
 
-    func uicolorFromHex(rgbValue:UInt32)->UIColor{
+   private  func uicolorFromHex(rgbValue:UInt32)->UIColor{
         let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
         let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
         let blue = CGFloat(rgbValue & 0xFF)/256.0
